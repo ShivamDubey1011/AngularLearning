bc8050ba48f84f6a2a1267c0f993ad9a4cca6015
Shivam Dubey <shivam.dubey@stits.co>
2022-05-02 13:03:23 +0530

    added array list in property and with using @input Decorator we are sending porperty attribute to it's child component property-card to display card

--- Client/src/app/property/property-card/property-card.component.ts
+++ Client/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,12 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  Property : any ={
-    "Id" :1,
-    "Name":"Birla House",
-    "Type": "House",
-    "Price" : 12000
-  }
+  /**
+   * with the help of @Input Decorator we are going to get value from parend component and then we are
+   *  using it in html to show the property .
+   */
+  @Input() Property : any;
   constructor() { }
 
   ngOnInit(): void {
--- Client/src/app/property/property-list/property-list.component.ts
+++ Client/src/app/property/property-list/property-list.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.scss']
 })
 export class PropertyListComponent implements OnInit {
-
+  properties : Array<any> =[
+    {
+    "Id" :1,
+    "Name":"Birla House",
+    "Type": "House",
+    "Price" : 12000
+  },
+  {
+    "Id" :2,
+    "Name":"Erose Villa",
+    "Type": "Villa",
+    "Price" : 14000
+  },
+  {
+    "Id" :3,
+    "Name":"Mark Hill",
+    "Type": "Flat",
+    "Price" : 12500
+  },
+  {
+    "Id" :4,
+    "Name":"Jp decs",
+    "Type": "Flat",
+    "Price" : 19000
+  },
+]
   constructor() { }
 
   ngOnInit(): void {
